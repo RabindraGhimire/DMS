6bfc6e01335b7f09161b3f3ce73bd626d3214366
Rabindra Ghimire <87578866+RabindraGhimire@users.noreply.github.com>
2026-02-23 23:20:51 +0100

    Initializing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        
+        <h1> Dolpo Management System</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
